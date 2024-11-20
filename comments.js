6b8e8cc86fd881eb23293718772347b0db46dfc5
Atharv Sanjeev Kumar <83223565+ATHARVSANJEEVKUMAR@users.noreply.github.com>
2024-11-20 15:31:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,17 @@
-// Create Web Server
-// Create a Node.js file that opens the requested file and returns the content to the client. If anything goes wrong, throw a 404 error:
-
-// Use the createServer method to create a web server:
-// Use the createReadStream method to read the requested file:
-// If the file is not found, return a 404 error:
-
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+  var filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  fs.access(filePath, fs.constants.F_OK, function(err) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      var readStream = fs.createReadStream(filePath);
+      readStream.pipe(res);
+    }
   });
 }).listen(8080);
